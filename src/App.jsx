d096488a4a15bd91b842b4360fffff5cfe92a9ad
andmana <iam.andmana@gmail.com>
2025-03-06 22:57:36 +0700

    Make edit side component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import MainPage from "./components/MainPage";
+import "./styles/app.css";
+import EditSide from "./components/forms/EditSide";
 function App() {
     return (
         <>
-            <MainPage />
+            <div className="app">
+                <nav className="top-nav">
+                    <h1>CV BUILDER</h1>
+                </nav>
+                <main className="content-container">
+                    <aside className="edit-side">
+                        <EditSide />
+                    </aside>
+                    <section className="resume-container"></section>
+                </main>
+                <footer className="footer"></footer>
+            </div>
         </>
     );
 }
---
+++ src/components/forms/EditSide.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "../../styles/edit-side.css";
+const EditSide = () => {
+    const [activeSection, setActiveSection] = useState(0);
+
+    const handleSectionEvent = (id) => {
+        setActiveSection(id);
+    };
+
+    return (
+        <>
+            <div className="template-loader">
+                <button className="clear-resume">Clear Resume</button>
+                <button className="load-template">Load Template</button>
+            </div>
+
+            <div className="edit-section">
+                <div className="edit-section-option">
+                    <ul>
+                        {EditSection.map((section) => {
+                            if (section.id === activeSection) return null; // Skip active section
+                            return (
+                                <li key={section.id}>
+                                    <button
+                                        onClick={() =>
+                                            handleSectionEvent(section.id)
+                                        }
+                                    >
+                                        {section.name}
+                                    </button>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+
+                <div className="active-edit-section">
+                    <h2>{EditSection[activeSection]?.name}</h2>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const EditSection = [
+    { id: 0, name: "Personal Detail" },
+    { id: 1, name: "Experience" },
+    { id: 2, name: "Education" },
+    { id: 3, name: "Skill" },
+];
+
+export default EditSide;
+
+// const EditSection = {
+//     0: "Personal Detail",
+//     1: "Experience",
+//     2: "Education",
+//     3: "Skill",
+// };
