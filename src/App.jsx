c233b4e925b4f4c11b041bdb063e74d0758b0f91
andmana <iam.andmana@gmail.com>
2025-03-06 23:15:00 +0700

    Add temporary  resume component view

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles/app.css";
 import EditSide from "./components/forms/EditSide";
+import Resume from "./components/resume/Resume";
 function App() {
     return (
         <>
@@ -11,7 +12,9 @@ function App() {
                     <aside className="edit-side">
                         <EditSide />
                     </aside>
-                    <section className="resume-container"></section>
+                    <section className="resume-container">
+                        <Resume />
+                    </section>
                 </main>
                 <footer className="footer"></footer>
             </div>
---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,24 @@
+import "../../styles/resume.css";
+
+const Resume = () => {
+    return (
+        <>
+            <div className="resume">
+                <div className="resume-top">
+                    <div className="resume-name">
+                        <h1>Andmana Permana Putra</h1>
+                    </div>
+                    <div className="contact-info">
+                        <div className="contact-mail">mail@mail.com</div>
+                        <div className="contact-phone">+62 2319051</div>
+                        <div className="contact-address">
+                            Jakarta, Indonesia
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Resume;
