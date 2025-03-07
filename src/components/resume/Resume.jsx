7dc2518e6d2cb412ef50896a68890bea0bb89b65
andmana <iam.andmana@gmail.com>
2025-03-07 10:16:38 +0700

    Add resume border and change some style

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -3,16 +3,18 @@ import "../../styles/resume.css";
 const Resume = () => {
     return (
         <>
-            <div className="resume">
-                <div className="resume-top">
-                    <div className="resume-name">
-                        <h1>Andmana Permana Putra</h1>
-                    </div>
-                    <div className="contact-info">
-                        <div className="contact-mail">mail@mail.com</div>
-                        <div className="contact-phone">+62 2319051</div>
-                        <div className="contact-address">
-                            Jakarta, Indonesia
+            <div className="resume-border">
+                <div className="resume">
+                    <div className="resume-top">
+                        <div className="resume-name">
+                            <h1>Andmana Permana Putra</h1>
+                        </div>
+                        <div className="contact-info">
+                            <div className="contact-mail">mail@mail.com</div>
+                            <div className="contact-phone">+62 2319051</div>
+                            <div className="contact-address">
+                                Jakarta, Indonesia
+                            </div>
                         </div>
                     </div>
                 </div>
