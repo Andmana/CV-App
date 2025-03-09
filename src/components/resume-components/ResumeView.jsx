afc0c0627b37c91438f4f26c9517c8f9dea996cb
andmana <iam.andmana@gmail.com>
2025-03-09 20:36:58 +0700

    Add extend button

--- src/components/resume-components/ResumeView.jsx
+++ src/components/resume-components/ResumeView.jsx
@@ -3,15 +3,33 @@ import "../../styles/resume.css";
 import ResumeSkill from "./ResumeSkill";
 import ResumeEducation from "./ResumeEducation";
 import ResumeExperience from "./REsumeExperience";
+import { useState } from "react";
 
 const ResumeView = ({ personalData, sectionData }) => {
+    const [useExtend, setExtend] = useState(false);
+
+    const handleExtend = () => {
+        setExtend(!useExtend);
+    };
+
     return (
-        <div className="resume-border">
-            <div className="resume">
-                <ResumeTop personalData={personalData} />
-                <ResumeSkill skills={sectionData.Skills} />
-                <ResumeEducation educations={sectionData.Educations} />
-                <ResumeExperience experiences={sectionData.Experiences} />
+        <div className={`resume-wrapper ${useExtend ? "extend" : ""}`}>
+            <div className="extend-button">
+                <button onClick={handleExtend}>
+                    <span
+                        className={`icon icon-${
+                            useExtend ? "shrink" : "extend"
+                        }`}
+                    ></span>
+                </button>
+            </div>
+            <div className="resume-border">
+                <div className="resume">
+                    <ResumeTop personalData={personalData} />
+                    <ResumeSkill skills={sectionData.Skills} />
+                    <ResumeEducation educations={sectionData.Educations} />
+                    <ResumeExperience experiences={sectionData.Experiences} />
+                </div>
             </div>
         </div>
     );
