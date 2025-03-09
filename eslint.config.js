e744a9e5386e6971a1a79e788861155f70840f35
andmana <iam.andmana@gmail.com>
2025-03-09 20:55:36 +0700

    REvert eslint change

--- eslint.config.js
+++ eslint.config.js
@@ -22,8 +22,8 @@ export default [
         },
         rules: {
             ...js.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            // "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+            // ...reactHooks.configs.recommended.rules,
+            "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
             "no-unused-vars": "off",
             "react-refresh/only-export-components": [
                 "warn",
